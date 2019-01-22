605b501301e50b3bb643bebf87187058426b1bd3
Bernard <Bernard@129.21.131.114>
2019-01-21 21:17:50 -0500

    Take input filename from the command line

--- hab/cbitmap/main.c
+++ hab/cbitmap/main.c
@@ -6,12 +6,40 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
 #include "bmp.h"
 
 int main(int argc, char *argv[]) {
 	struct d_bmp *myBmp = NULL;
-	myBmp = bmp_decode("C:/Users/Bernard/Documents/bmp_format1.bmp");
+	char *filename = NULL;
+	int rc;
+	int option_index = 0;
+
+	char *getoptOptions = "f:";
+	struct option long_options[] = {
+			{"file", required_argument, 0, 'f'},
+			{0, 0, 0, 0}
+	};
+	opterr = 1;
+
+	while ((rc = getopt_long_only(argc, argv, getoptOptions, long_options, &option_index)) != -1) {
+		switch (rc) {
+			case 'f':
+				filename = optarg;
+				break;
+			default:
+				printf ("Internal error: undefined option %0xX\n", rc);
+				exit(1);
+		}
+	}
+	if(NULL == filename) {
+		fprintf(stderr, "Usage: dbmp -f <filename>\n");
+		exit(1);
+	}
+
+	myBmp = bmp_decode(filename);
 	bmp_destroy(myBmp);
 	return 0;
 }
