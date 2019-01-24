31462419d4e6916199a5f5d239fe53bf41160428
Bernard <Bernard@129.21.131.114>
2019-01-24 02:45:19 -0500

    Added test case for DIB header size

--- hab/cbitmap/main.c
+++ hab/cbitmap/main.c
@@ -21,15 +21,17 @@ int main(int argc, char *argv[]) {
 	char *filename = NULL;
 	int rc;
 	int option_index = 0;
+	uint32_t expectedDIBHeaderSize = 0;
 	int32_t expectedWidth = 0, expectedHeight = 0;
 	uint16_t expectedPxSize = 0;
 
-	char *getoptOptions = "f:x:y:z:";
+	char *getoptOptions = "f:x:y:z:b:";
 	struct option long_options[] = {
 		{"file", required_argument, 0, 'f'},
 		{"expw", required_argument, 0, 'x'},
 		{"exph", required_argument, 0, 'y'},
 		{"pxsize", required_argument, 0, 'z'},
+		{"dibsize", required_argument, 0, 'b'},
 		{0, 0, 0, 0}
 	};
 	opterr = 1;
@@ -47,6 +49,9 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'z':
 				expectedPxSize = (uint16_t)strtol(optarg, &optarg, 10);
+				break;
+			case 'b':
+				expectedDIBHeaderSize = (uint32_t)strtol(optarg, &optarg, 10);
 				break;
 			default:
 				printf ("Internal error: undefined option %0xX\n", rc);
@@ -66,12 +71,16 @@ int main(int argc, char *argv[]) {
 	
 	myBmp = bmp_decode(filename);
 
+
+	if(expectedDIBHeaderSize) {
+		fprintf(stdout, "%u == dibsize  == dibHeadSize == %u:\t\t%s\n", expectedDIBHeaderSize, myBmp->header.dibHeadSize, lgcStr[myBmp->header.dibHeadSize == expectedDIBHeaderSize]);
+	}
 	if(expectedWidth && expectedHeight) {/* both are non-zero */
 		fprintf(stdout, "%d == width == expw == %d:\t\t%s\n", expectedWidth, myBmp->header.imgw, lgcStr[myBmp->header.imgw == expectedWidth]);
 		fprintf(stdout, "%d == height == exph == %d:\t\t%s\n", expectedHeight, myBmp->header.imgh, lgcStr[myBmp->header.imgh == expectedHeight]);
 	}
 	if(expectedPxSize) {
-		fprintf(stdout, "%d == pxSize_bits == pxsize == %d:\t%s\n", expectedPxSize, myBmp->header.pxSize_bits, lgcStr[myBmp->header.pxSize_bits == expectedPxSize]);
+		fprintf(stdout, "%u == pxSize_bits == pxsize == %u:\t%s\n", expectedPxSize, myBmp->header.pxSize_bits, lgcStr[myBmp->header.pxSize_bits == expectedPxSize]);
 	}
 	
 	bmp_destroy(myBmp);
